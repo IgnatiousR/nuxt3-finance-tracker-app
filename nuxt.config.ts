1029786f1010dfeb1d37b9537d924786f27b953c
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 22:10:41 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/ui',
-    '@nuxt/fonts'
-  ]
-})
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@nuxt/ui", "@nuxt/fonts"],
+  css: ["~/assets/css/main.css"],
+  // ui: {
+  //   colorMode: false,
+  // },
+});
