062c9647cedbdcd2f9bdeb9a433eeb5d6cbe1d06
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-14 22:10:44 +0600

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex h-screen flex-col">
+    <div class="w-full border-b">Header</div>
+    <div class="max-w-7xl lg:mx-auto p-5 md:px-10 w-full flex-1">
+      <UButton class="cursor-pointer">Button</UButton>
+    </div>
+    <div>Footer</div>
+  </div>
+</template>
