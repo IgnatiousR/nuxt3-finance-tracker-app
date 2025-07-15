4647f9f481a9aac5f558a57ef3873c3ba3caf657
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-15 22:14:42 +0600

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,35 +1,9 @@
 <template>
-  <div class="container mx-auto max-w-7xl">
-    <main class="my-10">
+  <div class="flex h-screen flex-col">
+    <AppHeader />
+    <main class="wrapper flex-1">
       <slot />
     </main>
+    <AppFooter />
   </div>
 </template>
-
-<!-- <script setup>
-// Link to Google Fonts
-useHead({
-  link: [
-    {
-      rel: "preconnect",
-      href: "https://fonts.googleapis.com",
-    },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Inter&display=swap",
-      crossorigin: "",
-    },
-  ],
-});
-</script>
-
-<style>
-/* Set the font family for the entire site */
-body {
-  font-family: "Inter";
-}
-/* Set the background color for the entire site */
-body {
-  @apply dark:bg-gray-900 bg-white;
-}
-</style> -->
