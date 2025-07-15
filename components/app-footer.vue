86925e3fc5ecf7e62ca34e8f1b94bd6b479faa4b
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-15 22:14:37 +0600

    Create app-footer.vue

---
+++ components/app-footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="border-t border-neutral-700">
+    <div class="p-5 flex-center">
+      2025 &nbsp;<Icon name="tdesign:copyright" />&nbsp; APP. All Rights
+      Reserved
+    </div>
+  </footer>
+</template>
