64ee18aa77ac8b5aa7343746770c37b772b8821a
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-16 15:52:23 +0600

    Update app-footer.vue

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="border-t border-neutral-700">
     <div class="p-5 flex-center">
-      2025 &nbsp;<Icon name="tdesign:copyright" />&nbsp; APP. All Rights
+      2025 &nbsp;<UIcon name="tdesign:copyright" />&nbsp; APP. All Rights
       Reserved
     </div>
   </footer>
