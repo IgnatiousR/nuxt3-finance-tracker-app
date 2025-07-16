14da4399c7055674f8acfabc544dd1e0f65a39ab
Ignatious Rozario <ignatiousr80@gmail.com>
2025-07-16 17:18:50 +0600

    Update app-footer.vue

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -1,8 +1,12 @@
 <template>
   <footer class="border-t border-neutral-700">
     <div class="p-5 flex-center">
-      2025 &nbsp;<UIcon name="tdesign:copyright" />&nbsp; APP. All Rights
+      {{ currentYear }} &nbsp;<UIcon name="tdesign:copyright" />&nbsp; APP. All Rights
       Reserved
     </div>
   </footer>
 </template>
+
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
